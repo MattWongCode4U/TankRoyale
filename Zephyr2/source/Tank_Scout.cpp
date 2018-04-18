9f52cc4775caccb3b8e74fdda6bc47b45b830e0f
Denis Turitsa <dturitsa@gmail.com>
2018-04-18 01:16:08 -0700

    hopefully fix not destroyed bug

--- Zephyr2/source/Tank_Scout.cpp
+++ Zephyr2/source/Tank_Scout.cpp
@@ -69,8 +69,12 @@ void Tank_Scout::shoot(int targetX, int targetY) {
 		//deal damage to closest tank;
 		if (currClosestTank != nullptr) {
 			currClosestTank->takeDamage(damage);
-			if(currClosestTank->health <= 0)
+			if (currClosestTank->health <= 0) {
 				gameSystemUtil->postMessageToBus(new Msg(UPDATE_OBJ_SPRITE, currClosestTank->id + ",1,crater.png,"));
+				currClosestTank->collisionsEnabled = false;
+				currClosestTank->renderable = "crater.png";
+			}
+				
 		}
 	}
 }
