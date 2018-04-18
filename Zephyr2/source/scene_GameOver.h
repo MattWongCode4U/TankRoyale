c210fbbcb86a125a0c6af8c16b2e5e34bf44e015
John Janzen <joalja2002@msn.com>
2018-04-18 15:00:38 -0700

    Documentation
    Scene Game Over

--- Zephyr2/source/scene_GameOver.h
+++ Zephyr2/source/scene_GameOver.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Scene.h"
-
+/*
+* A scene transitioned to when the player is killed or when the player wins.
+* It allocates the needed scene data and the leaderboard.
+* Redirects to main menu when retreat button is clicked.
+*/
 class Scene_GameOver : public Scene {
 public:
 	Scene_GameOver(MessageBus* _mbus, GameSystem* _gs);
@@ -12,6 +16,7 @@ public:
 	//runs whenever a message is received by GameSystem
 	void sceneHandleMessage(Msg * msg);
 
+	// Populates the leaderboard table
 	void PopulateTable(const std::vector<std::string> & vec);
 
 	//code that runs once when  scene is loaded goes here
